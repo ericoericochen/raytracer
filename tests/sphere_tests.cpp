162ead1689c9dd1050da8633d2c4e338b46a4fc6
Eric Chen <deeplearningericchen@gmail.com>
2024-06-11 20:33:24 -0700

    test cases for sphere

--- tests/sphere_tests.cpp
+++ tests/sphere_tests.cpp
@@ -9,8 +9,31 @@ int main()
     auto direction = tuple::vec(0, 0, 1);
     auto r = Ray(origin, direction);
     auto s = Sphere();
-
     auto xs = sphere::intersects(s, r);
 
     assert(xs.size() == 2 && xs[0] == 4.0 && xs[1] == 6.0);
+
+    origin = tuple::point(0, 2, -5);
+    direction = tuple::vec(0, 0, 1);
+    r = Ray(origin, direction);
+    s = Sphere();
+    xs = sphere::intersects(s, r);
+
+    assert(xs.size() == 0);
+
+    origin = tuple::point(0, 0, 0);
+    direction = tuple::vec(0, 0, 1);
+    r = Ray(origin, direction);
+    s = Sphere();
+    xs = sphere::intersects(s, r);
+
+    assert(xs.size() == 2 && xs[0] == -1.0 && xs[1] == 1.0);
+
+    origin = tuple::point(0, 0, 5);
+    direction = tuple::vec(0, 0, 1);
+    r = Ray(origin, direction);
+    s = Sphere();
+    xs = sphere::intersects(s, r);
+
+    assert(xs.size() == 2 && xs[0] == -6.0 && xs[1] == -4.0);
 }
